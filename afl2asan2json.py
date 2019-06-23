88a90f70d1acce6fbac76eeb8275f38c247d2e98
AMatchandaHaystack <51268526+AMatchandaHaystack@users.noreply.github.com>
2019-06-23 00:33:25 -0400

    Update afl2asan2json.py

--- afl2asan2json.py
+++ afl2asan2json.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+/usr/bin/python3
 
 import os
 import time
+import mmap
 import shutil
 import base64
 import subprocess
@@ -11,6 +12,7 @@ directory1 = "/A2A2J"
 directory2 = "/A2A2J/ASANLOGS/"
 directory3 = "/A2A2J/DUPL_CRASH_FILES/"
 directory4 = "/A2A2J/COMMAND_LOGS/"
+directory5 = "/A2A2J/JSON_BLOCK_FILES/"
 
 #Nifty terminal trick for display colors.
 #RED
@@ -30,6 +32,7 @@ Y = YEL + "[!]" + END
 BLU = '\033[94m'
 
 
+print ( Y + "Requires jq tool.")
 
 #Let's start building our work directories. Hit enter to just skip this all if they exist.
 buildDirs = raw_input(R + "Create work directories? y/n/anything else to skip ")
@@ -45,11 +48,13 @@ elif os.path.isdir(directory1) is False and buildDirs == "y":
 	print(G + "Creating work directory: /A2A2J/ASANLOGS/")
 	print(G + "Creating work directory: /A2A2J/COMMAND_LOGS/")
 	print(G + "Creating work directory: /A2A2J/DUPL_CRASH_FILES/")
+	print(G + "Creating work directory: /A2A2J/JSON_BLOCK_FILES/")
 #Make them.
 	os.mkdir(directory1, 0755)
 	os.mkdir(directory2, 0755)
 	os.mkdir(directory3, 0755)
 	os.mkdir(directory4, 0755)
+	os.mkdir(directory5, 0755)
 	print(G + "Created directories.\n")
 else:
 #Blank input and/or skip.
@@ -103,9 +108,9 @@ generateASANLOG = raw_input(R + "Generate ASANLOGS from ASAN Binary? y/n ")
 if generateASANLOG == "y":
 #Build script to generate ASAN logs.
 	if switchOps == "":
-		bashCommand = "for i in " + str(directory3) + "*; do " + str(asanBinary) + " $i; done"
+		bashCommand = "for i in " + str(directory3) + "*; do " + str(asanBinary) + " $i && echo FILE$i >>" + directory2 + "ASANLOG.txt; done"
 	else:
-		bashCommand = "for i in " + str(directory3) + "*; do " + str(asanBinary) + " " + switchOps + " $i; done"
+		bashCommand = "for i in " + str(directory3) + "*; do " + str(asanBinary) + " " + str(switchOps) + " $i && FILEecho $i >>" + directory2 + "ASANLOG.txt; done"
 	print (Y + "Built your script as: " + BLU + str(bashCommand) + str(switchOps) + END + ".")
 	print (Y + "Please review carefully.")
 	print (Y + "You may need to modify the bash command in a2a2j source (bashCommand variable) to match your particular binary.\n")
@@ -134,6 +139,43 @@ if generateASANLOG == "y":
 	executeScript = "/bin/bash " + str(directory4) + str(fileName) + ".sh"
 	print(G + "Generating!")
 	os.system(executeScript + " 1> /dev/null 2>" + str(directory2) + "/ASANLOG.txt ; echo \"FINISHED_FINISHED_FINISHED\" >> " + str(directory2) + "ASANLOG.txt")
-	print(G + "Done!")
+	print(G + "Done!\n")
 else:
 	print (Y + "Skipped.")
+
+crashLog = "/A2A2J/ASANLOG/ASANLOG.txt"
+#JSON time
+#If our log exists, open it.
+if os.path.isfile(crashLog):
+#Read about this being a little easier on memory overhead in case we have like 50,000 crash files.
+	validityCheck = open(str(crashLog))
+	s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+#Check for a known string in AddressSanitizer logs.
+	if s.find(b'AddressSanitizer:') != -1:
+    	print(G + 'We have successfully found our AddressSanitizer strings in our ASANLOG!')
+    	validityCheck.close()
+    else:
+    	print (Y + "Something went wrong with our logs. We can't find an AddressSanitizer string in them to parse.")
+    	validityCheck.close()
+    	exit(1)
+else:
+	print (Y + "Nothing at" + BLU + str(crashLog) + "?" + END)
+	exit(1)
+
+print(G + "Successfully found AddressSanitizer strings in " + BLU + str(crashLog) + END + "\n")
+createJSON = raw_input(R + "Parse logs to JSON payloads in" + BLU + directory5 + END + " ? y/n ")
+if createJSON == "y":
+	print(G + "Parsing " + BLU + crashLog + END + "...")
+#AWK magic makes a file for each match in the ASAN output pattern.
+	os.chdir(directory5)
+	individualLogs = "awk -v RS=\"ERROR\" 'NR > 1 {print RS $0 > (NR-1); close(NR-1)}' <" + str(crashLog)
+	os.system(individualLogs)
+	print(G + "Built block files. Converting to JSON to be parsed by endpoint.")
+	print(Y + "This can take a minute because of the number of files and cleanup. Please wait.")
+#We convert each file to a JSON format.
+	asan2JSON = "for i in $(ls); do jq -R -s -c 'split(\"\n\")' $i > $i.json; done && find . -type f  ! -name \"*.json\"  -delete" 
+	os.system(asan2JSON)
+	print(G + "Individual JSON block files based on ASAN logs from AFL's crash files are now ready to be sent upstream to your API!")
+	exit(1)
+else:
+	exit(1)
